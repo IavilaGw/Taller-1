586357e3c9afb896bd1582b3391f354524f936e0
IavilaGw <i.avilag@uniandes.edu.co>
2023-10-22 21:56:51 -0500

    Taller 2

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,28 @@
 import { dataSeries } from "./data.js";
 var seriesTbody = document.getElementById("series");
 var averageSeasonsElm = document.getElementById("average-seasons");
+var cardImagen = document.getElementById("imagen_serie");
+var cardTitulo = document.getElementById("titulo_serie");
+var cardDescripcion = document.getElementById("descripcion_serie");
+var cardEnlace = document.getElementById("enlace_serie");
 renderSeriesInTable(dataSeries);
 averageSeasonsElm.innerHTML = "".concat(getAverageSeasons(dataSeries));
 function renderSeriesInTable(series) {
     console.log("Desplegando series");
     series.forEach(function (serie) {
         var trElement = document.createElement("tr");
-        trElement.innerHTML = "<td>".concat(serie.indice, "</td>\n                           <td>").concat(serie.name, "</td>\n                           <td>").concat(serie.channel, "</td>\n                           <td>").concat(serie.seasons, "</td>");
+        trElement.className = "serie_tabulada";
+        trElement.innerHTML = "<td>".concat(serie.indice, "</td>\n    <td>").concat(serie.name, "</td>\n    <td>").concat(serie.channel, "</td>\n    <td>").concat(serie.seasons, "</td>");
+        var createClickHandler = function (row) {
+            return function () {
+                cardImagen.src = "".concat(serie.imagen);
+                cardTitulo.innerHTML = "".concat(serie.name);
+                cardDescripcion.innerHTML = "".concat(serie.description);
+                cardEnlace.href = "".concat(serie.enlace);
+                cardEnlace.innerHTML = "".concat(serie.enlace);
+            };
+        };
+        trElement.onclick = createClickHandler(trElement);
         seriesTbody.appendChild(trElement);
     });
 }
